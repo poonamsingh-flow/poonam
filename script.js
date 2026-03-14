aa6dd859d91107711328d526d4b8f9fb5cd1a6bf
poonam singh <poonamsingh76645@gmail.com>
2026-03-14 12:38:07 +0530

    Initial frontend project

---
+++ script.js
@@ -0,0 +1,8 @@
+function showName(){
+
+const name=document.getElementById("nameInput").value;
+
+document.getElementById("output").innerText=
+"Hello "+name+" welcome to my deployed website!";
+
+}
